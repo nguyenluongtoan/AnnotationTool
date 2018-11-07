1e17b39a5b2b78d09bb0fe5d8aa55053de4a4cd0
DuongNX <duongnx.work@gmail.com>
2018-11-07 12:24:46 +0700

    Add jquery and show image

--- Project/annotationToolLqa/src/app/control/control.component.ts
+++ Project/annotationToolLqa/src/app/control/control.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as $ from 'jquery';
 @Component({
-  selector: 'app-control',
-  templateUrl: './control.component.html',
-  styleUrls: ['./control.component.css']
+	selector: 'app-control',
+	templateUrl: './control.component.html',
+	styleUrls: ['./control.component.css']
 })
 export class ControlComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+	widthOld = 100; // %
+	heightOld = 100;
+	listImages = [];
+	constructor() { }
+	ngOnInit() {}
+	DrawImage(width,height){
+		var canvas = document.getElementById("myCanvas");
+		var ctx = canvas.getContext("2d");
+		var img = new Image();
+		img.src = $("#imageCar").attr("src");
+		ctx.save();
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		ctx.restore();
+		setTimeout(function () {
+			ctx.drawImage(img, 0, 0,img.width * width/100,img.height * height/100);
+			ctx.strokeRect(0, 0,img.width * width/100,img.height * height/100);
+		}, 100);
+		this.widthOld = width;
+		this.heightOld = height;
+	}
+	ShowImage(obj){
+		console.log(obj)
+		console.log(obj.fakePath)
+		$("#imageCar").attr("src",obj.fakePath);
+		this.DrawImage(this.widthOld,this.heightOld);
+	}
+	ShowListImage(){
+		this.listImages= JSON.parse($("#fileUpload").val())
+	}
 }
