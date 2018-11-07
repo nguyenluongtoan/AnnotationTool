20c5cae22a68a5533e4cff0cebe6c2464cb14be3
DuongNX <duongnx.work@gmail.com>
2018-11-07 11:24:16 +0700

    chuyen code component cu sang project

--- Project/annotationToolLqa/src/app/app.module.ts
+++ Project/annotationToolLqa/src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ImageViewComponent } from './image-view/image-view.component';
 import { ControlComponent } from './control/control.component';
+import { BatComponent } from './bat/bat.component';
+import { CatComponent } from './cat/cat.component';
+import { RatComponent } from './rat/rat.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ImageViewComponent,
-    ControlComponent
+    ControlComponent,
+    BatComponent,
+    CatComponent,
+    RatComponent
   ],
   imports: [
     BrowserModule,
